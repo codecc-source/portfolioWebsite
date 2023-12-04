61eb6cb966f74de79624850f36389284ee2f8c54
codecc-source <carlitotingson@gmail.com>
2023-12-05 03:00:42 +0800

    animation to hovers, logo, added base content

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeSlide, setActiveSlide] = useState('HOME');
+
+  const handleButtonClick = (slide) => {
+    setActiveSlide(slide);
+  };
+
   return (
     <div>
       <div className="container-fluid">
         <div className="row">
-          <div className="black-bg toplinks">
-            <img src="%PUBLIC_URL%/images/logo.png" alt="Logo" style={{ width: '25%' }} />
-            <a href="#">HOME</a>
-            <a href="#">WORKS</a>
-            <a href="#">ABOUT</a>
-            <a href="#">CONTACT</a>
+          <div className="black-bg d-flex align-items-center">
+            <div className='col-lg-6'>
+              <img src="../images/logo.png" alt="Logo" style={{ width: '12%' }} />
+            </div>
+            <div className='col-lg-6 d-flex justify-content-end'>
+              <button className='toplinks' onClick={() => handleButtonClick('HOME')}>HOME</button>  {/* add clicked animation for each button */}
+              <button className='toplinks' onClick={() => handleButtonClick('WORKS')}>WORKS</button>
+              <button className='toplinks' onClick={() => handleButtonClick('ABOUT')}>ABOUT</button>
+              <button className='toplinks' onClick={() => handleButtonClick('CONTACT')}>CONTACT</button> 
+            </div>
           </div>
         </div>
       </div>
 
       <div>
-        <div className="socialLinks text-center">
-          <div>
-            <a style={{ color: 'rgb(215, 89, 246)' }}>Contact Us @</a>
-            <a href="#">http://FacebookCompanyLink</a>
-            <a href="#">http://InstagramCompanyLink</a>
-            <a href="#">http://GMailCompanyLink</a>
-            <a href="#">http://LinkedInCompanyLink</a>
-          </div>
+        {activeSlide === 'HOME' && <div className='placeholderShiz'>Content for HOME</div>}
+        {activeSlide === 'WORKS' && <div className='placeholderShiz'>Content for WORKS</div>}
+        {activeSlide === 'ABOUT' && <div className='placeholderShiz'>Content for ABOUT</div>}
+        {activeSlide === 'CONTACT' && <div className='placeholderShiz'>Content for CONTACT</div>}
+      </div>
+
+      <div className="socialLinks text-center">
+        <div>
+          <a style={{ color: 'rgb(215, 89, 246)' }}>Contact Us @</a>
+          <a href="#">http://FacebookCompanyLink</a>
+          <a href="#">http://InstagramCompanyLink</a>
+          <a href="#">http://GMailCompanyLink</a>
+          <a href="#">http://LinkedInCompanyLink</a>
         </div>
       </div>
     </div>
