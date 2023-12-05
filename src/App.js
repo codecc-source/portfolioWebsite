36fe6a9cbbcfd865d53577557b1e9ce2b5584f94
codecc-source <carlitotingson@gmail.com>
2023-12-06 01:35:40 +0800

    Changed navigation bar and implemented MUI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+import NavigationBar from './NavigationBar';
+import { Box } from '@mui/material';
 
 function App() {
   const [activeSlide, setActiveSlide] = useState('HOME');
@@ -8,40 +12,132 @@ function App() {
     setActiveSlide(slide);
   };
 
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+      console.log(container);
+  }, []);
+
   return (
     <div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="black-bg d-flex align-items-center">
-            <div className='col-lg-6'>
-              <img src="../images/logo.png" alt="Logo" style={{ width: '12%' }} />
-            </div>
-            <div className='col-lg-6 d-flex justify-content-end'>
-              <button className='toplinks' onClick={() => handleButtonClick('HOME')}>HOME</button>  {/* add clicked animation for each button */}
-              <button className='toplinks' onClick={() => handleButtonClick('WORKS')}>WORKS</button>
-              <button className='toplinks' onClick={() => handleButtonClick('ABOUT')}>ABOUT</button>
-              <button className='toplinks' onClick={() => handleButtonClick('CONTACT')}>CONTACT</button> 
+    <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                background: {
+                    color: {
+                        value: "#0d47a1",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+          />
+          <Box sx={{backgroundColor: "black"}}>
+            <NavigationBar/>
+          </Box> 
+          
+
+          <div  style={{zIndex : 1000, display: 'flex'}}>
+            <div>
+         {/* <div className="container-fluid">
+          <div className="row">
+            <div className="black-bg d-flex align-items-center" style={{width: '100%'}}>
+              <div className='col-lg-6'>
+                <img src="../images/logo.png" alt="Logo" style={{ width: '12%' }} />
+              </div>
+              <div className='col-lg-6 d-flex justify-content-end'>
+                <button className='toplinks' onClick={() => handleButtonClick('HOME')}>HOME</button> 
+                <button className='toplinks' onClick={() => handleButtonClick('WORKS')}>WORKS</button>
+                <button className='toplinks' onClick={() => handleButtonClick('ABOUT')}>ABOUT</button>
+                <button className='toplinks' onClick={() => handleButtonClick('CONTACT')}>CONTACT</button> 
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div>
-        {activeSlide === 'HOME' && <div className='placeholderShiz'>Content for HOME</div>}
-        {activeSlide === 'WORKS' && <div className='placeholderShiz'>Content for WORKS</div>}
-        {activeSlide === 'ABOUT' && <div className='placeholderShiz'>Content for ABOUT</div>}
-        {activeSlide === 'CONTACT' && <div className='placeholderShiz'>Content for CONTACT</div>}
-      </div>
-
-      <div className="socialLinks text-center">
         <div>
-          <a style={{ color: 'rgb(215, 89, 246)' }}>Contact Us @</a>
-          <a href="#">http://FacebookCompanyLink</a>
-          <a href="#">http://InstagramCompanyLink</a>
-          <a href="#">http://GMailCompanyLink</a>
-          <a href="#">http://LinkedInCompanyLink</a>
+          {activeSlide === 'HOME' && <div className='placeholderShiz'>Content for HOME</div>}
+          {activeSlide === 'WORKS' && <div className='placeholderShiz'>Content for WORKS</div>}
+          {activeSlide === 'ABOUT' && <div className='placeholderShiz'>Content for ABOUT</div>}
+          {activeSlide === 'CONTACT' && <div className='placeholderShiz'>Content for CONTACT</div>}
+        </div>  */}
+
+        <div className="socialLinks text-center">
+          <div>
+            <a style={{ color: 'rgb(215, 89, 246)' }}>Contact Us @</a>
+            <a href="#">FacebookCompanyLink</a>
+            <a href="#">InstagramCompanyLink</a>
+            <a href="#">GMailCompanyLink</a>
+            <a href="#">LinkedInCompanyLink</a>
+          </div>
         </div>
       </div>
+          </div>
     </div>
   );
 }
