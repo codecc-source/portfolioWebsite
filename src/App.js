e4f7ea55d79e5c03ef27f809b537154b13ee8f3f
codecc-source <carlitotingson@gmail.com>
2024-01-20 03:36:02 +0800

    edited animations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import NavigationBar from './NavigationBar';
-import { Box } from '@mui/material';
+import {Box} from '@mui/material';
 import BackgroundParticles from './BackgroundParticles';
 import Works from './pages/Works';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import HomeDefault from './pages/HomeDefault';
 import AnimatedCursor from 'react-animated-cursor';
-import { motion, AnimatePresence } from 'framer-motion';
+import {motion, AnimatePresence} from 'framer-motion';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('HOME');
   const [showCursor, setShowCursor] = useState(true);
+  const [isImageVisible, setIsImageVisible] = useState(false);
 
   const handlePageClick = (page) => {
     setCurrentPage(page);
-  };
+ };
 
   const renderPageContent = () => {
     switch (currentPage) {
@@ -28,36 +29,39 @@ function App() {
         return <Contact />;
       default:
         return <HomeDefault />;
-    }
-  };
+   }
+ };
 
   useEffect(() => {
     const handleResize = () => {
       setShowCursor(window.innerWidth > 700);
-    };
+   };
 
     handleResize();
     window.addEventListener('resize', handleResize);
 
     return () => {
       window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+   };
+ }, []);
 
-  let isImageVisible = true;
-  function imgInterval() {
-    var myImage = document.getElementById("myImage");
+  useEffect(() => {
+    const imgTimeout = setTimeout(() => {
+      setIsImageVisible(true);
 
-    if (isImageVisible) {
-      myImage.style.display = "none";
-      isImageVisible = false;
-    } else {
-      myImage.style.display = "block";
-      isImageVisible = true;
-    }
-  }
+      const hideTimeout = setTimeout(() => {
+        setIsImageVisible(false);
+     }, 5000);
 
-  setInterval(imgInterval, 20000);
+      return () => {
+        clearTimeout(hideTimeout);
+     };
+   }, 60000); //to set when the image appears after Xseconds
+
+    return () => {
+      clearTimeout(imgTimeout);
+   };
+ }, []);
 
   return (
     <div>
@@ -84,40 +88,42 @@ function App() {
           ]}
         />
       )}
-      <Box sx={{ height: '100vh', width: '100vw', position: 'relative' }}>
-        <div style={{ zIndex: 1, width: '100%', height: '100%', overflow: 'scroll' }}>
+      <Box sx={{height: '100vh', width: '100vw', position: 'relative'}}>
+        <div style={{zIndex: 1, width: '100%', height: '100%', overflow: 'scroll'}}>
           <BackgroundParticles />
 
-          <Box sx={{ backgroundColor: 'black' }}>
+          <Box sx={{backgroundColor: 'black'}}>
             <NavigationBar onPageClick={handlePageClick} />
           </Box>
 
-          <Box sx={{ position: 'relative', zIndex: 2 }}>
+          <Box sx={{position: 'relative', zIndex: 2}}>
             <AnimatePresence>
               <motion.div
                 key={currentPage}
-                initial={{ width: 0 }}
-                animate={{ width: '100%' }}
-                exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+                initial={{width: 0}}
+                animate={{width: '100%'}}
+                exit={{x: window.innerWidth, transition: {duration: 0.1}}}
               >
                 {renderPageContent()}
-                <motion.img
-                  id='myImage'
-                  src='/portfoliowebsite/images/red.png'
-                  style={{
-                    width: '100px',
-                    height: '100px',
-                    position: 'absolute',
-                    top: '50%',
-                    left: 0, 
-                  }}
-                  alt='red'
-                  animate={{
-                    left: window.innerWidth - 100,
-                    rotate: 360, 
-                    transition: { duration: 5, ease: 'linear', repeat: Infinity },
-                  }} 
-                />
+                {isImageVisible && (
+                  <motion.img
+                    id='myImage'
+                    src='/portfoliowebsite/images/red.png'
+                    style={{
+                      width: '100px',
+                      height: '100px',
+                      position: 'absolute',
+                      top: '50%',
+                      left: 0,
+                   }}
+                    alt='red'
+                    animate={{
+                      left: window.innerWidth - 100,
+                      rotate: 360,
+                      transition: {duration: 5, ease: 'linear'},
+                   }}
+                  />
+                )}
               </motion.div>
             </AnimatePresence>
           </Box>
